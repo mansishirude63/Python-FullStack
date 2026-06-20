c3ea8b6fba24580b181983b4f050a3289d6f0f44
Mansi Shirude <mansishirude63@gmail.com>
2026-06-20 13:17:29 +0530

    Enhance weather data display with emojis
    Updated weather display with emoji for temperature, humidity, and weather condition.

--- Frontend/weatherApp/script.js
+++ Frontend/weatherApp/script.js
@@ -10,10 +10,9 @@ async function getData() {
 function renderData(data) {
     document.getElementById("result").innerHTML = `
     <h1> ${data.name} (<i>${data.sys.country}</i>)</h1>
-    <p>Temperature : <b> ${data.main.temp}<sup>o</sup>C </b></p>
-    <p>Humidity : <b> ${data.main.humidity}</b></p>
-    <p>${data.weather[0].main}: <b>${data.weather[0].description}. </b></p> 
-    
+    <p>🌡️Temperature : <b> ${data.main.temp}<sup>o</sup>C </b></p>
+    <p>💧Humidity : <b> ${data.main.humidity}</b></p>
+    <p>⛅${data.weather[0].main}: <b>${data.weather[0].description}. </b></p> 
     `
 }
 document.getElementById("btnSubmit").addEventListener('click', getData)
